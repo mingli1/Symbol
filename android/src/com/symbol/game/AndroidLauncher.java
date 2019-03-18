32f0cb6a85a043c5793873dd92a91a57f3656f19
Ming Li <mli@C02V60PBHV2R.local>
2019-03-18 16:09:27 -0400

    reformat

--- android/src/com/symbol/game/AndroidLauncher.java
+++ android/src/com/symbol/game/AndroidLauncher.java
@@ -6,10 +6,12 @@ import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class AndroidLauncher extends AndroidApplication {
+
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new Symbol(), config);
 	}
+
 }
